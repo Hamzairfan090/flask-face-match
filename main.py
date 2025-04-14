801c7d4248fb918f149c11f389566c8e8ccd69d3
Hamzairfan090 <168353448+Hamzairfan090@users.noreply.github.com>
2025-04-14 15:53:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -99,4 +99,5 @@ if __name__ == '__main__':
     print("\n===============================")
     print("Flask server starting on port 5000...")
     print("===============================\n")
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
